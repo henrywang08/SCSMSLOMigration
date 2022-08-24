41ed54f708f08ebe77d7a287598521d2d24d5d75
henrywang08 <henry_ly@hotmail.com>
2022-08-24 20:51:58 +0800

    Update the script

--- Migrate-SCSMSLO.ps1
+++ Migrate-SCSMSLO.ps1
@@ -2,15 +2,22 @@
 
 Pre-requisites: 
 
-1. Workitem Queues must be migrated to SCSM using MP import
+1. Workitem Queues must be migrated to SCSM 2019 using MP import
 2. Calendar objects must be migrated. A simple Orchestrator runbook is created for this. 
 3. Metric objects must be migrated. A simple Orchestrator runbook is created for this. 
+
+I have created an Orchestrator Runbook to migrate SLO, but System.SLA.Group and System.SLA.WorkflowTarget 
+are group object, which needs to select specific Related Claess name in Get Reletionship activity. 
+
+Powershell script may help to resolve this issue with New-SCRelationshipInstance 
 
 Here is how the script works:
 
 1. Get SLO from SCSM 2012 R2 with all related objects
 2. Create SLO in SCSM 2019
-3. Create all relationships based on existing objects in SCSM 2012 R2
+3. Create all related relationships
+
+
 
 
 
@@ -32,7 +39,7 @@ $sm19SLOs = Get-SCSMClassInstance -Class $slaconfigclass -ComputerName $sm19serv
 foreach ($slo in $SLOs)
 {
     
-    if ($sm19SLOs.displayname -ccontains  $slo.displayname)
+    if ($sm19SLOs.displayname -contains  $slo.displayname)
     {
         Write-Host "SLO $($slo.displayname) is already migrated to SCSM 2019"
     }
@@ -58,76 +65,52 @@ foreach ($slo in $SLOs)
     # It is not necessary for now.    
     # [threading.thread]::CurrentThread.CurrentCulture = 'en-US'
 
-
-# Create SLO object in SCSM 2019
         New-SCSMClassInstance -Class $sm19slaclass `
                             -Property $sm19sloproperty `
                             -ComputerName $sm19server
                          
-# Get the created SLO object from SCSM 2019. Should add verification in case failed to create.         
+        
         $sm19SLO = Get-SCSMClassInstance -Class $sm19slaclass | ? DisplayName -eq $slo.DisplayName
 
-# Get all related object instances from SCSM 2012 R2 for the migrating SLO
         $slorels = Get-SCSMRelationshipInstance -SourceInstance $slo -ComputerName $sm12server
-        
-<# The relationships from one SLO object. 
-
-$slorel | % {Get-SCSMRelationship -id $_.relationshipid}
-
-DisplayName : 
-Source      : System.SLA.Configuration
-Target      : System.SLA.WorkflowTarget
-
-DisplayName : Calendar
-Source      : System.SLA.Configuration
-Target      : System.Calendar
-
-DisplayName : Target
-Source      : System.SLA.Configuration
-Target      : System.SLA.Target
-
-DisplayName : Warning Threshold
-Source      : System.SLA.Configuration
-Target      : System.SLA.Target
-
-DisplayName : 
-Source      : System.SLA.Configuration
-Target      : System.SLA.Group
-
-DisplayName : Metric
-Source      : System.SLA.Configuration
-Target      : System.SLA.Metric
-
-#>
-        
         foreach ($rel in $slorels)
         {
+          if ($rel.IsDeleted -eq $false)
+          {
             $relobj = $rel.TargetObject
             Write-Host "Related Object from SCSM 2012 $($relobj.displayname) for SLO $($slo.displayname)"
 
+                     
             $relclass = Get-SCSMRelationship -id $rel.RelationshipId -ComputerName $sm12server
             Write-Host $relclass
 
-# Seems redundant code here.  
-<#
-       
-            $targetclass = Get-SCSMClass -id $relclass.Target.Type.id -ComputerName $sm12server
+
+            $targetclass = Get-SCSMClass -Instance $relobj -ComputerName $sm12server
             Write-Host $targetclass.DisplayName
-            
-            $sm12target = Get-SCSMClassInstance -class $targetclass -ComputerName $sm12server `
+ 
+ #   Let's try to use #FullName to identify objects
+         
+ #           if ($relclass.DisplayName -eq 'Metric')
+            {
+  #                $sm12target = Get-SCSMClassInstance -class $targetclass -ComputerName $sm12server `
+  #                          | ? '#FullName' -eq $($relobj.FullName)
+            }
+ <#
+           else
+ 
+            {
+                $sm12target = Get-SCSMClassInstance -class $targetclass -ComputerName $sm12server `
                             | ? displayname -eq $($relobj.displayname)
+            }
 #>
-
-            $sm12target = $relobj
 # Need to get the real class since the previous one from relationship is abstract.                 
-            $targetclass = Get-SCSMClass -Instance $sm12target
+ #           $targetclass = Get-SCSMClass -Instance $sm12target -ComputerName $sm12server
 
 
-# The 'Target' and 'Warning Threshold' objects have some special relationshiop with SLO, similar as Manual Activity 
-# to Service Request. They need to be created directly with relationship. 
             if (($relclass.DisplayName -eq 'Target') -or ($relclass.DisplayName -eq 'Warning Threshold'))
             {
-                
+                $sm12target = Get-SCSMClassInstance -class $targetclass -ComputerName $sm12server `
+                          | ? '#FullName' -eq $($relobj.FullName)
  
                 $targetdisplayname = $sm12target.Displayname
                 $targetid = $sm12target.Id
@@ -144,18 +127,27 @@ Target      : System.SLA.Metric
                 New-SCRelationshipInstance -RelationshipClass $relclass -Source $sm19SLO -TargetClass $targetclass `
                          -TargetProperty $targetproperty -ComputerName $sm19server 
             }
- # Other related objects can be attached with relationship. 
             else
             {
                 $sm19targetobj = Get-SCSMClassInstance -Class $targetclass -ComputerName $sm19server `
-                                | ? Id -eq $($sm12target.id)
+                                | ? '#FullName' -eq $($relobj.FullName)
                 Write-Host "Get same target object on SCSM 2019 $($sm19targetobj.DisplayName)"
 
-                New-SCRelationshipInstance -RelationshipClass $relclass -Source $sm19SLO -Target $sm19targetobj `
+            
+
+            New-SCRelationshipInstance -RelationshipClass $relclass -Source $sm19SLO -Target $sm19targetobj `
                               -ComputerName $sm19server
 
             }
+
             Write-Host
+          }
         }
+
+
+
+
+
     }
+
 }
